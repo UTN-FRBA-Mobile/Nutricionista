ff8cc81534e8c32895260331f5d741e0f8928398
DiegoaQuiros <diegoquiros@est.frba.utn.edu.ar>
2019-05-04 18:59:48 -0300

    Navigation Template

--- app/src/main/java/com/utn/nutricionista/HomeActivity.kt
+++ app/src/main/java/com/utn/nutricionista/HomeActivity.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 
     fun buttonPressed(view: View) {
-        val intent = Intent(this, HomeActivity::class.java)
+        val intent = Intent(this, DietActivity::class.java)
         startActivity(intent)
     }
 }
