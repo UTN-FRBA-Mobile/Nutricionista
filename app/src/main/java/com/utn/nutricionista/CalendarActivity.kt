6fc9a06c010070863744afcd491904243b708284
DiegoaQuiros <diegoquiros@est.frba.utn.edu.ar>
2019-05-04 18:59:48 -0300

    Navigation Template

--- app/src/main/java/com/utn/nutricionista/CalendarActivity.kt
+++ app/src/main/java/com/utn/nutricionista/CalendarActivity.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class CalendarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_calendar)
     }
 
     fun buttonPressed(view: View) {
-        val intent = Intent(this, HomeActivity::class.java)
+        val intent = Intent(this, MyProgressActivity::class.java)
         startActivity(intent)
     }
 }
