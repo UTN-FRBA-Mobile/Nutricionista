c114a8cfaac55c4331258f86ff21e537bf957b54
DiegoaQuiros <diegoquiros@est.frba.utn.edu.ar>
2019-05-04 18:59:48 -0300

    Navigation Template

--- app/src/main/java/com/utn/nutricionista/DietActivity.kt
+++ app/src/main/java/com/utn/nutricionista/DietActivity.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class DietActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_diet)
     }
 
     fun buttonPressed(view: View) {
-        val intent = Intent(this, HomeActivity::class.java)
+        val intent = Intent(this, CalendarActivity::class.java)
         startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/utn/nutricionista/MyProgressActivity.kt
@@ -0,0 +1,12 @@
+package com.utn.nutricionista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyProgressActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_progress)
+    }
+}
