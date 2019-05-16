0c22b4b8f6b1fedee51f200b916e99b783674ce9
Ivan <imiandrusic@gmail.com>
2019-05-15 23:24:30 -0300

    Inicial

---
+++ app/src/main/java/com/utn/nutricionista/DetalleComidaActivity.kt
@@ -0,0 +1,12 @@
+package com.utn.nutricionista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalleComidaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_comida)
+    }
+}
