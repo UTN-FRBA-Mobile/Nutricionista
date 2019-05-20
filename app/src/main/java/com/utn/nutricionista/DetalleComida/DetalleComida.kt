40bec2eef67cbaabae5217057bd3054c5cce23c6
IvanMiandrusic <imiandrusic@gmail.com>
2019-05-20 08:47:25 -0300

    Avance lista de alimentos

---
+++ app/src/main/java/com/utn/nutricionista/DetalleComida/DetalleComida.kt
@@ -0,0 +1,7 @@
+package com.utn.nutricionista.DetalleComida
+
+data class DetalleComida(var detalle : String,
+                         var cantidad: Int,
+                         var enPreDef: Boolean = false){
+
+}
