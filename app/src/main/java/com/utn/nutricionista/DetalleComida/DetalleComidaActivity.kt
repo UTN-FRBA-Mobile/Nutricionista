defcc838e8243ddf80291b6fb7bcd4bde9fa34f0
IvanMiandrusic <imiandrusic@gmail.com>
2019-05-20 08:47:25 -0300

    Avance lista de alimentos

---
+++ app/src/main/java/com/utn/nutricionista/DetalleComida/DetalleComidaActivity.kt
@@ -0,0 +1,36 @@
+package com.utn.nutricionista.DetalleComida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.utn.nutricionista.R
+
+class DetalleComidaActivity : AppCompatActivity(),
+    DetalleComidaFragment.OnListFragmentInteractionListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_comida)
+        supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.dietaPropuestaFragmentContainer,
+                DetalleComidaFragment.newInstance()
+            )
+            .commit()
+        supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.dietaExtraFragmentContainer,
+                DetalleComidaFragment.newInstance()
+            )
+            .commit()
+
+    }
+
+    override fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.dietaPropuestaFragmentContainer, fragment)
+            .commit()
+    }
+
+}
