6685261f73b2c62c4e6adf9bd682e366a73e6c88
IvanMiandrusic <imiandrusic@gmail.com>
2019-05-22 00:02:54 -0300

    Listas de comida - avance

--- app/src/main/java/com/utn/nutricionista/DetalleComida/DetalleComidaFragment.kt
+++ app/src/main/java/com/utn/nutricionista/DetalleComida/DetalleComidaFragment.kt
@@ -17,6 +17,9 @@ import com.utn.nutricionista.R
  */
 class DetalleComidaFragment : Fragment() {
 
+    val DIETA_PREDEF = 1
+    val FUERA_DIETA_PREDEF = 2
+
     private var listener: OnListFragmentInteractionListener? = null
     private lateinit var detalleComidaAdapter: DetalleComidaAdapter
 
@@ -25,20 +28,35 @@ class DetalleComidaFragment : Fragment() {
         super.onStart()
 
         //Datos Mockeados --> obtenidos desde la BD
-        val dietaPreDefArr = ArrayList<String>()
-        dietaPreDefArr.add("Medialunas")
-        dietaPreDefArr.add("Tazas de Cafe")
-        dietaPreDefArr.add("Te")
-        dietaPreDefArr.add("Tostadas")
-        dietaPreDefArr.add("Galletitas dulces")
+        val dietaPreDefArr:  List<DetalleComida> = arrayListOf(
+            DetalleComida("Medialunas", 2),
+            DetalleComida("Tazas de Cafe", 1),
+            DetalleComida("Tostadas", 2),
+            DetalleComida("Galletitas dulces", 3),
+            DetalleComida("Te", 2)
+        )
 
-        val dietaRealArr = ArrayList<DetalleComida>()
-        dietaRealArr.add(DetalleComida("Tostadas", 2))
-        dietaRealArr.add(DetalleComida("Medialunas", 2))
-        dietaRealArr.add(DetalleComida("Tazas de Cafe", 1))
-        dietaRealArr.add(DetalleComida("Jugo de Naranja", 1))
+        //Datos Mockeados --> obtenidos desde la BD
+        var dietaRealArr: List<DetalleComida> = arrayListOf(
+            DetalleComida("Tostadas", 2),
+            DetalleComida("Medialunas", 2),
+            DetalleComida("Tazas de Cafe", 1),
+            DetalleComida("Jugo de Naranja", 1)
+        )
 
-        dietaRealArr.map{item -> if(item.detalle in dietaPreDefArr) item.enPreDef = true;}
+
+        val tipoDieta: Int = this.arguments!!.getInt("tipoDieta")
+        when(tipoDieta){
+            DIETA_PREDEF -> {
+                dietaRealArr = dietaPreDefArr.onEach { item -> if(item.detalle in dietaRealArr.map { e -> e.detalle }) item.tipoItem = DIETA_PREDEF }
+
+            }
+            FUERA_DIETA_PREDEF -> {
+                dietaRealArr = dietaRealArr.filter{ item -> item.detalle !in dietaPreDefArr.map { e->e.detalle } }.onEach { e -> e.tipoItem=FUERA_DIETA_PREDEF }
+            }
+            else ->{
+            }
+        }
 
         detalleComidaAdapter.setData(dietaRealArr)
         detalleComidaAdapter.notifyDataSetChanged()
@@ -60,7 +78,6 @@ class DetalleComidaFragment : Fragment() {
             adapter = detalleComidaAdapter
         }
     }
-
 
 
     override fun onAttach(context: Context) {
@@ -102,7 +119,22 @@ class DetalleComidaFragment : Fragment() {
          * @return A new instance of fragment MainFragment.
          */
         @JvmStatic
-        fun newInstance() = DetalleComidaFragment()
+        fun newExtraDietaInstance(): DetalleComidaFragment {
+            val newFragment = DetalleComidaFragment()
+            val args = Bundle()
+            args.putInt("tipoDieta", 2)
+            newFragment.arguments = args
+            return newFragment
+        }
+
+        @JvmStatic
+        fun newPreDefDietaInstance(): DetalleComidaFragment {
+            val newFragment = DetalleComidaFragment()
+            val args = Bundle()
+            args.putInt("tipoDieta", 1)
+            newFragment.setArguments(args)
+            return newFragment
+        }
     }
 
 }
