009191a190303af4b452a5548c36db1b41f6656c
Diegoaquiros <diegoaquiros@gmail.com>
2019-06-02 17:46:47 -0300

    chat fixes and improvements

--- app/src/main/java/com/utn/nutricionista/RecyclerAdapter.kt
+++ app/src/main/java/com/utn/nutricionista/RecyclerAdapter.kt
@@ -4,22 +4,35 @@ import android.graphics.Typeface
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.recyclerview_message.view.*
+import kotlinx.android.synthetic.main.recyclerview_own_message.view.*
 
 class RecyclerAdapter(val messages: ArrayList<Message>):  RecyclerView.Adapter<RecyclerAdapter.MessageHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.MessageHolder {
-        val inflatedView = parent.inflate(R.layout.recyclerview_message, false)
-        return MessageHolder(inflatedView)
+        if (viewType == 1) {
+            val inflatedView = parent.inflate(R.layout.recyclerview_own_message, false)
+            return MessageHolder(inflatedView)
+        } else {
+            val inflatedView = parent.inflate(R.layout.recyclerview_others_message, false)
+            return MessageHolder(inflatedView)
+        }
     }
 
     override fun getItemCount(): Int {
         return messages.size
     }
 
+    override fun getItemViewType(position: Int): Int {
+        if (messages[position].own) {
+            return 1
+        } else {
+            return 2
+        }
+    }
+
     override fun onBindViewHolder(holder: RecyclerAdapter.MessageHolder, position: Int) {
         val itemPhoto = messages[position]
-        holder.bindPhoto(itemPhoto)
+        holder.bindMessage(itemPhoto)
     }
 
     class MessageHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
@@ -34,7 +47,7 @@ class RecyclerAdapter(val messages: ArrayList<Message>):  RecyclerView.Adapter<R
 
         }
 
-        fun bindPhoto(message: Message) {
+        fun bindMessage(message: Message) {
             this.message = message
             view.itemUserName.text = message.sender + ":"
             view.itemUserName.setTypeface(null, Typeface.BOLD)
@@ -45,4 +58,4 @@ class RecyclerAdapter(val messages: ArrayList<Message>):  RecyclerView.Adapter<R
     }
 }
 
-data class Message(val sender: String, val text: String, val date: String, val status: String)
+data class Message(val sender: String, val text: String, val date: String, val status: String, val own: Boolean)
