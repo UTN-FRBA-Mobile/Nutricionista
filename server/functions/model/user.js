729d49368d6edbb2d8e6c3282d53d4de0193c7d1
Julián Berbel Alt <julianberbel@hotmail.com>
2019-06-08 19:13:31 -0300

    Add http API and onUserCreate functions

---
+++ server/functions/model/user.js
@@ -0,0 +1,21 @@
+const admin = require('firebase-admin');
+const _ = require('lodash');
+
+function User(doc) {
+  _.assign(this, doc.data());
+  this.dbRef = doc.ref; 
+}
+
+User.prototype = {
+  update: function (updates) {
+    _.assign(this, updates);
+    return this.dbRef.update(updates);
+  }
+};
+
+User.create = async function (data) {
+  const ref = await db.collection('usuarios').add(data);
+  return new User({ref: ref, data: () => data});
+}
+
+module.exports = User;
