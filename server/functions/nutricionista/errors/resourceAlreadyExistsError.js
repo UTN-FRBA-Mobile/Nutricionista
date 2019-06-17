c4a9abb5a529a4a2f0529250968454978e864161
Julián Berbel Alt <julianberbel@hotmail.com>
2019-06-17 19:06:20 -0300

    Add api diet GET and POST endpoints

---
+++ server/functions/nutricionista/errors/resourceAlreadyExistsError.js
@@ -0,0 +1,9 @@
+const ValidationError = require('./validationError');
+
+class ResourceAlreadyExistsError extends ValidationError {
+  constructor(){
+    super("Resource already exists");
+  }
+}
+
+module.exports = ResourceAlreadyExistsError;
