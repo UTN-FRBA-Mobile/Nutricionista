d3bdaaf48967d632f16000fc83411ab6ef0773a3
Julián Berbel Alt <julianberbel@hotmail.com>
2019-06-17 22:49:16 -0300

    Add PUT diet endpoint

--- server/functions/nutricionista/api/endpoints/diet.js
+++ server/functions/nutricionista/api/endpoints/diet.js
@@ -1,6 +1,7 @@
 const Diet            = require('../../model/diet');
 const ValidationError = require('../../errors/validationError');
 const { success }     = require('../helpers/response');
+const { db }               = require('../../firebase')
 
 module.exports = (app) => {
   app.get('/diet', async (req, res) => {
@@ -20,6 +21,16 @@ module.exports = (app) => {
       throw e;
     }
 
+    return success(res);
+  });
+
+  app.put('/diet', async (req, res) => {
+    const data = req.body;
+    const snap = await db.collection(`usuarios/${req.currentUser.uid}/dieta`).where('fecha', '==', data.fecha).get();
+    
+    if(snap.empty) return res.status(404).send('Not Found');
+
+    snap.docs[0].ref.update(data);
     return success(res);
   });
 };
