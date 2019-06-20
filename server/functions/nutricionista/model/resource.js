f29a3d62c3ef0f87dd1afe14eb6ef173042b652e
Julián Berbel Alt <julianberbel@hotmail.com>
2019-06-20 02:49:49 -0300

    RESTify persistence too

--- server/functions/nutricionista/model/resource.js
+++ server/functions/nutricionista/model/resource.js
@@ -1,11 +1,17 @@
+const { db }                                = require('../firebase');
 const { NotFoundError, UnimplementedError } = require('../errors');
 const _                                     = require('lodash');
 
 class Resource {
   constructor(doc) {
     _.assign(this, doc.data());
-    this.dbRef = doc.ref;
     this.id = doc.ref.id;
+  }
+
+  static async index(uid) {
+    const results = await this.collectionRef.where('uid', '==', uid).get();
+
+    return results.docs.map((data) => new this(data));
   }
 
   static async get(id) {
@@ -16,13 +22,30 @@ class Resource {
     return new this(doc);
   }
 
-  static get collectionRef() {
-    throw new UnimplementedError('collectionRef');
+  static async create(data) {
+    await this.validate(data);
+
+    const ref = await this.collectionRef.add(data);
+    return new this({ ref: ref, data: () => data });
   }
 
   update(updates) {
     _.assign(this, updates);
     return this.dbRef.update(updates);
+  }
+
+  static async validate(_data) {} // for overriding purposes
+
+  get dbRef() {
+    return this.constructor.collectionRef.doc(this.id);
+  }
+
+  static get collectionRef() {
+    return db.collection(this.collection);
+  }
+
+  static get collection() {
+    throw new UnimplementedError('collection');
   }
 }
 
