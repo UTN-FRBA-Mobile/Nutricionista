93055685e879882cfaec7a11dd833fbf0fce0d39
Julián Berbel Alt <julianberbel@hotmail.com>
2019-06-20 01:06:47 -0300

    Add errors export and move get up inheritance chain

--- server/functions/nutricionista/model/resource.js
+++ server/functions/nutricionista/model/resource.js
@@ -1,10 +1,23 @@
-const _ = require('lodash');
+const { NotFoundError, UnimplementedError } = require('../errors');
+const _                                     = require('lodash');
 
 class Resource {
   constructor(doc) {
     _.assign(this, doc.data());
     this.dbRef = doc.ref;
     this.id = doc.ref.id;
+  }
+
+  static async get(id) {
+    const doc = await this.collectionRef.doc(id).get();
+
+    if(!doc.exists) throw new NotFoundError(this.name);
+
+    return new this(doc);
+  }
+
+  static get collectionRef() {
+    throw new UnimplementedError('collectionRef');
   }
 
   update(updates) {
