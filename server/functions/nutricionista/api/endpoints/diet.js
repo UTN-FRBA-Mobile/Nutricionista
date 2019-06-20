217dc611fd69c986a7aea8da0327551c88064dbd
Julián Berbel Alt <julianberbel@hotmail.com>
2019-06-20 18:21:13 -0300

    Extract common endpoint logic elsewhere

--- server/functions/nutricionista/api/endpoints/diet.js
+++ server/functions/nutricionista/api/endpoints/diet.js
@@ -1,33 +1,4 @@
-const Diet                    = require('../../model/diet');
-const { success, withErrors } = require('../helpers/response');
+const common = require('./common');
+const Diet   = require('../../model/diet');
 
-module.exports = (app) => {
-  app.get('/diet', withErrors(async (req, res) => {
-    const diets = await Diet.index(req.currentUser.uid);
-
-    return success(res, diets);
-  }));
-
-  app.get('/diet/:id', withErrors(async (req, res) => {
-    const diet = await Diet.get(req.params.id);
-
-    return success(res, diet);
-  }));
-
-  app.post('/diet', withErrors(async (req, res) => {
-    const data = req.body;
-    data.uid = req.currentUser.uid;
-
-    await Diet.create(data);
-
-    return success(res);
-  }));
-
-  app.put('/diet/:id', withErrors(async (req, res) => {
-    const diet = await Diet.get(req.params.id)
-
-    await diet.update(req.body)
-
-    return success(res);
-  }));
-};
+module.exports = common(Diet);
