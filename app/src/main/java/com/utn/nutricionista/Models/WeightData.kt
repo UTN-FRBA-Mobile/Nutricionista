72b90fd8b8824af0adcc40c2a826490042d852fd
fmassarini <Hexacta19>
2019-06-27 11:05:44 -0300

    fix input datepicker

--- app/src/main/java/com/utn/nutricionista/Models/WeightData.kt
+++ app/src/main/java/com/utn/nutricionista/Models/WeightData.kt
@@ -1,5 +1,7 @@
 package com.utn.nutricionista.models
 
 import java.time.LocalDate
-
+//import kotlinx.serialization.Serializable
+//
+//@Serializable
 class WeightData(var id:  Int, var userId : Int, var weight: Double, var date : LocalDate)
