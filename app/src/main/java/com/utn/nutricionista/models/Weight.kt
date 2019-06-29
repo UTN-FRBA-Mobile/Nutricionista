6ca080bec978ae2456d7b4f306fbf5aacb39417e
fmassarini <famassarini@gmail.com>
2019-06-29 03:59:58 -0300

    post weight record

--- app/src/main/java/com/utn/nutricionista/models/Weight.kt
+++ app/src/main/java/com/utn/nutricionista/models/Weight.kt
@@ -1,7 +1,7 @@
 package com.utn.nutricionista.models
 import java.time.LocalDate
 
-class Weight(var id:  String,
+class Weight(var id:  String?,
              var uid : String? = null,
              var peso: Double,
              var fecha : String)
