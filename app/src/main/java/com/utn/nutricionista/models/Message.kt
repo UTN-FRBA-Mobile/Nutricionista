aafc81eec4ee253bf3698fd901ffb4c639c96862
Diegoaquiros <diegoaquiros@gmail.com>
2019-06-30 18:36:37 -0300

    localization

--- app/src/main/java/com/utn/nutricionista/models/Message.kt
+++ app/src/main/java/com/utn/nutricionista/models/Message.kt
@@ -1,5 +1,6 @@
 package com.utn.nutricionista.models
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 data class Message(val id: String?, val sender: String, val text: String, val date: Date, val status: MessageStatus, val own: Boolean) {
@@ -9,6 +10,11 @@ data class Message(val id: String?, val sender: String, val text: String, val da
 
     fun getErrorMessage(): Message {
         return Message(null, this.sender, this.text, this.date, MessageStatus.ERROR, this.own)
+    }
+
+    fun getFormattedDate(): String {
+        val format = SimpleDateFormat("yyyy/MM/dd hh:mm")
+        return format.format(date)
     }
 }
 
