3e87db8db0344bb56d9fc4bbc83e0ba28d318260
Julián Berbel Alt <julianberbel@hotmail.com>
2019-06-30 21:42:32 -0300

    Fix crash on login

--- app/src/main/java/com/utn/nutricionista/InputWeightDialogFragment.kt
+++ app/src/main/java/com/utn/nutricionista/InputWeightDialogFragment.kt
@@ -27,7 +27,7 @@ class InputWeightDialogFragment : DialogFragment(), DatePickerDialog.OnDateSetLi
             date.setOnClickListener { showDatePickerDialog() }
 
             btnCancel = view.findViewById(R.id.btnInputWeightCancel) as Button
-            btnCancel.setOnClickListener { dialog.dismiss()}
+            btnCancel.setOnClickListener { dialog?.dismiss() }
             btnAccept = view.findViewById(R.id.btnInputWeightConfirm) as Button
             btnAccept.setOnClickListener { saveNewWeightRecord() }
 
@@ -39,7 +39,7 @@ class InputWeightDialogFragment : DialogFragment(), DatePickerDialog.OnDateSetLi
     private fun saveNewWeightRecord() {
         if (weight.text.isNotEmpty() && date.text.isNotEmpty()) {
             (activity as WeightActivity).saveNewWeightRecord(weight.text.toString().toFloat(), date.text.toString())
-            dialog.dismiss()
+            dialog?.dismiss()
         }
     }
 
